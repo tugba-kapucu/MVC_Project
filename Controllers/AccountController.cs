1e2d9c47e6bc96774b623077eff4b68642de9e1e
tugbademir <tugbademir@pro-minds.com>
2022-11-21 00:43:19 +0300

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProje.Models;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace MvcProje.Controllers
+{
+    public class AccountController : Controller
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserDetail user)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (Roles.RoleExists("user"))
+                    {
+                        Roles.CreateRole("user");
+                        WebSecurity.CreateUserAndAccount(user.UserName, user.Password, new { user.Address, user.Email, user.FirsName, user.Surname });
+                        return RedirectToAction("Index","Home");
+                    }
+                }
+                catch (MembershipCreateUserException ex)
+                {
+                    return RedirectToAction("Error", new { errorMessage = ex.Message });
+                }
+            }
+            return View(user);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserDetail user)
+        {
+            if (WebSecurity.Login(user.UserName,user.Password))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View("Error", "Kullanıcı Adı ya da Parola Yanlış!");
+            }
+        }
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult UserDetail()
+        {
+            var currentUser = db.UserDetails.Find(WebSecurity.CurrentUserId);
+            return View(currentUser);
+        }
+        public ActionResult Error(string errorMessage)
+        {
+            ViewBag.Message = errorMessage;
+            return View();
+        }
+    }
+}
+ 
---
+++ Controllers/ProductManagerController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProje.Models;
+
+namespace MvcProje.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        private NORTHWNDEntities db = new NORTHWNDEntities();
+
+        //
+        // GET: /ProductManager/
+
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        //
+        // GET: /ProductManager/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // GET: /ProductManager/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
+            return View();
+        }
+
+        //
+        // POST: /ProductManager/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        //
+        // GET: /ProductManager/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        //
+        // POST: /ProductManager/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        //
+        // GET: /ProductManager/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
